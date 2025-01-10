562984bc7c9d2d4127ab1cfb42ad764a2df5f5c5
fidelis182 <fedymueni18@gmail.com>
2025-01-10 06:24:28 +0300

    created blog page

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+// sproduct page
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
+
+/*Understand the function*/
+/*if you click the small image it replaces the main iage*/
+smallimg[0].onclick = function(){
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    MainImg.src = smallimg[3].src;
+}
